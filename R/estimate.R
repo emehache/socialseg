cecb51889927f052bc29f3b1ba5881768054e58c
emehache <manuel.hernandez@fcea.edu.uy>
2024-02-20 09:05:10 -0300

    correcciones

--- R/estimate.R
+++ R/estimate.R
@@ -6,33 +6,40 @@
 #' @return A data.table
 #' @export
 
-estimate <- function(input, vars, lx = 100, ly, Ngrid, smoothing = "none", sigma, graficar = F, verbose = T) {
+estimate <-  function(input, vars, lx = 100, ly, Ngrid, smoothing = "none", sigma = "none", indices = c("D", "H"), graficar = F, verbose = T) {
 
-  if (smoothing == 'none') {
-    input@data <- input@data[, vars]
-    est <- spseg(input)
-    return(est)
-    }
 
   if (smoothing == "equal") {
-    data <- input %>%
+    datos <- input %>%
       distribuir(lx = lx, vars = vars, verbose = verbose)
   }
 
   if (smoothing == "kernel") {
-    data <- input %>%
+    datos <- input %>%
       distribuir(lx = lx, vars = vars, verbose = verbose) %>%
       suavizar(sigma = sigma)
   }
 
-  est <- data %>%
-    na.omit %>%
-    spseg(x = .[,c("x","y")],
-          data = .[, vars, with = F],
-          method = "all",
-          smoothing = "none")
-    # {c(D = .@d, R = .@r,H = .@h)}
+  if (smoothing == 'none') {
+    input@data <- input@data[, vars]
+    est <- spseg(input)
+  } else {
+    est <- datos %>%
+      na.omit %>%
+      spseg(x = .[,c("x","y")],
+            data = .[, vars, with = F],
+            method = "all",
+            smoothing = "none")
+  }
 
-  return(est)
+  out <- est@p %>%
+    as.data.table(keep.rownames = "var1") %>%
+    melt.data.table(id = "var1", variable.name = "var2") %>%
+    .[, .(index = 'P', coef = paste(var1, var2, sep = "-"), value)] %>%
+    rbind(data.table(index = c("D", "R", "H"), coef = NA, value = c(est@d, est@r, est@h)), .) %>%
+    # cbind(lx, smoothing, sigma, vars = paste0(vars, collapse="+")) %>%
+    .[index %chin% indices]
+
+  return(out)
 
 }
